23c20ef7d3a89a10f7f9939ad4841719f25a50b1
Homesttead <152499403+Homesttead@users.noreply.github.com>
2024-02-04 00:07:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -32,6 +32,8 @@ class CardassianDowekBot(discord.Client):
         if message.content.lower().startswith(f"{self.prefix}stats"):
             await self.show_current_value(message.channel)
 
+        await self.process_commands(message)
+
     async def update_coin_value(self):
         while True:
             await asyncio.sleep(self.update_interval)
